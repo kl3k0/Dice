965eb21bd52e0ad25e5cf7ba2959f11b2125232b
kl3k0 <a.klekocinska@gmail.com>
2022-11-25 19:40:39 +0100

    button to be clicked, no more refresh for roll

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-var randomNumber1 = Math.floor(Math.random() *6) +1;
-var randomNumber2 = Math.floor(Math.random() *6) +1;
+function dicesPlsWork() {
+  var randomNumber1 = Math.floor(Math.random() *6) +1;
+  var randomNumber2 = Math.floor(Math.random() *6) +1;
 
-var diceImage1 = "images/dice" + randomNumber1 +".png";
-document.querySelector(".img1").src = diceImage1;
+  var diceImage1 = "images/dice" + randomNumber1 +".png";
+  document.querySelector(".img1").src = diceImage1;
 
-var diceImage2 = "images/dice" + randomNumber2 +".png";
-document.querySelector(".img2").src = diceImage2;
+  var diceImage2 = "images/dice" + randomNumber2 +".png";
+  document.querySelector(".img2").src = diceImage2;
 
-if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").textContent = "🚩 Player 1 wins!"
-} else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").textContent = "Player 2 wins! 🚩"
-} else {
-  document.querySelector("h1").textContent = "You're both right!"
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").textContent = "🚩 Player 1 wins!"
+  } else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").textContent = "Player 2 wins! 🚩"
+  } else {
+    document.querySelector("h1").textContent = "You're both right!"
+  }
 }
